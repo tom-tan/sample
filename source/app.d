a269042b5c503c0c900edfff964fa881ebd580b3
Tomoya Tanjo <ttanjo@gmail.com>
2016-12-03 22:06:18 +0900

    Remove dependency for dlangui

--- source/app.d
+++ source/app.d
@@ -1,13 +1,15 @@
-import dlangui;
+// import dlangui;
 
-mixin APP_ENTRY_POINT;
+// mixin APP_ENTRY_POINT;
 
-extern(C) int UIAppMain(string[] args)
-{
-    auto window = Platform.instance.createWindow("sample", null, WindowFlag.Resizable,
-                                                 300, 300);
-    window.mainWidget = new FrameLayout;
-    window.show;
+// extern(C) int UIAppMain(string[] args)
+// {
+//     auto window = Platform.instance.createWindow("sample", null, WindowFlag.Resizable,
+//                                                  300, 300);
+//     window.mainWidget = new FrameLayout;
+//     window.show;
 
-    return Platform.instance.enterMessageLoop();
-}
+//     return Platform.instance.enterMessageLoop();
+// }
+
+void main() {}
