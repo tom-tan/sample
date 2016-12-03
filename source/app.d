ba3b0e4d3c95f6fae1bdb533d84546f0ff9eaa56
Tomoya Tanjo <ttanjo@gmail.com>
2016-12-04 00:51:05 +0900

    Add entrypoint for dlangui

--- source/app.d
+++ source/app.d
@@ -1,15 +1,15 @@
 import dlangui;
 
-// mixin APP_ENTRY_POINT;
+mixin APP_ENTRY_POINT;
 
-// extern(C) int UIAppMain(string[] args)
-// {
-//     auto window = Platform.instance.createWindow("sample", null, WindowFlag.Resizable,
-//                                                  300, 300);
-//     window.mainWidget = new FrameLayout;
-//     window.show;
+extern(C) int UIAppMain(string[] args)
+{
+    // auto window = Platform.instance.createWindow("sample", null, WindowFlag.Resizable,
+    //                                              300, 300);
+    // window.mainWidget = new FrameLayout;
+    // window.show;
 
-//     return Platform.instance.enterMessageLoop();
-// }
+    // return Platform.instance.enterMessageLoop();
+    return 0;
+}
 
-void main() {}
